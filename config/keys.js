4c3edbca2afc61c00fcf241fb5bbc04615251e0e
WeaChris <aekfc45@gmail.com>
2021-06-16 18:32:52 +0300

    third and actual commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI  :"mongodb+srv://metepeter:<password>@board-cluster.2nysk.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority\n" ,
+    secret:"mongodb+srv://metepeter:<password>@board-cluster.2nysk.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority\n"
+}
--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+// Bring in the Database Config and connect with the database
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
+}).then(() => {
+    console.log(`Database connected successfully ${db}`)
+}).catch(err => {
+    console.log(`Unable to connect with the database ${err}`)
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
