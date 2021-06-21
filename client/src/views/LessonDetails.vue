782b96efdf04fc95bc453e5d6345fabb1a73ed67
Petros Evangelos Triantafyllis <metrospa@hotmail.com>
2021-06-22 00:22:33 +0300

    pame kala

---
+++ client/src/views/LessonDetails.vue
@@ -0,0 +1,47 @@
+<template>
+    <form>
+        <div class="well">
+            <h2>{{currentlesson.title}}</h2><a>{{currentlesson.sem}}</a>
+            <h3 >{{currentlesson.teacher.name+" "+currentlesson.teacher.lastname}}</h3>
+            <p>{{currentlesson.desc}}</p>
+
+
+        </div>
+      
+    </form>
+
+
+</template>
+
+<script>
+import {
+    mapActions,
+    mapGetters
+} from "vuex"
+
+
+export default {
+    
+    computed:{
+        ...mapGetters(["currentlesson"])
+        }, 
+
+    methods: {
+        ...mapActions(["getLessonById", "getLessonAuthors"])
+
+    },
+    beforeMount() {
+        this.getLessonById(this.$route.params.id);
+
+    },
+
+    data: function () {
+        return {}
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
